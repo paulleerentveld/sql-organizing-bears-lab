7723951abfc34e622e6725cc015fb3fb5afe3650
paulleerentveld <paul@veldnet.com.au>
2022-07-19 18:52:08 +1000

    lab done

---
+++ lib/insert.sql
@@ -0,0 +1,27 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'angry', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Rowdy', 5, 'F', 'brown', 'happy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Tabitha', 2, 'M', 'brown', 'angry', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Sergeant Brown', 8, 'F', 'sad', 'goofy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Melissa', 6, 'M', 'brown', 'goofy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Grinch', 7, 'M', 'brown', 'hungry', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Wendy', 5, 'M', 'brown', 'angry', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 5, 'M', 'brown', 'angry', 0);
+
+
+
+
